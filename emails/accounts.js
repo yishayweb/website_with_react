51e3adb70272dbdd2f21c4bca3e71de114117d71
Yishayf <yishayf@gmail.com>
2019-10-04 22:38:42 +0300

    switched sendgrid accounts file again

--- emails/accounts.js
+++ emails/accounts.js
@@ -6,9 +6,9 @@ const sendEmailSendGrid = (email_obj, callback) => {
   console.log("snding email");
   sgMail.send({
     to: 'yishayweb@gmail.com',
-    from: 'yishayweb@gmail.com',
+    from: 'admin@yishayonweb.com',
     subject: 'Hello from yishayonweb.com',
-    text: 'This is to email from the website from the new'
+    text: 'This is to email from the website from the new dev'
   })
 }
 
